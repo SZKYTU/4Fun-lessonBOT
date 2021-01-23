86be6539f9eaa1ce66d274ac66f2ce76d2fcaeda
Piotr Czupyk <szkycode@gmail.com>
2021-01-23 16:49:59 +0100

    regex titledate WORK!

--- volcan_message.py
+++ volcan_message.py
@@ -1,5 +1,5 @@
+import re
 import datetime
-import itertools
 from take_cert import client
 
 
@@ -8,16 +8,23 @@ today = datetime.date.today()
 
 
 class Message:
-    def __init__(self, name, content):
+    def __init__(self, name, content, titledate):
         self.name = name
         self.content = content
+        self.titledate = re.search(r"\d\d.\d\d", titledate).group()
 
 
 mess = client.get_messages(yesterday, today)
 tesm = []
 for el in mess:
-    tesm.append(Message(el.title, el.content))
+    tesm.append(Message(el.title, el.content, el.title))
 
-for tesms in tesm:
-    print(tesms.name, "\n")
-    print(tesms.content, "\n")
+# for tesms in tesm:
+#     # print(tesms.name, "\n")
+#     # print(tesms.titledate)
+#     print(tesms.content, "\n")
+
+# test = tesm[0].name
+# x = reg.search(test)
+# print(x.group())
+# print(type(x.group()))
